a1f022f420ffaf2676203a7c6321b9b3bab839c8
David J. Hammaker <davidjosh93@gmail.com>
2019-01-22 00:22:01 -0500

    Move printed files to a new folder
    To allow the script to be run multiple times without printing the same 
    files over again, the script sends printed files to a new folder titled 
    'printed_<today's date>' within the same directory as the files. A 10s 
    timer delays the movement of files after each print to ensure that files 
    are not moved before they've had a chance to be printed.

--- pdf_printer.py
+++ pdf_printer.py
@@ -1,11 +1,26 @@
 import os
+import shutil
 from glob import glob
 from datetime import date
+import time
 
 
 if __name__ == '__main__':
     with open('paths.txt', 'r') as paths:
         for path in paths:
+            files_discovered = False
+            all_files = []
+            new_home = f'{path}'[:-1] \
+                       + f'/printed_{date.today()}'
             for file in glob(f'{path}'[:-1] + '*'):
                 if file.endswith('.pdf'):
-                    os.startfile(file, 'print')
+                    if not files_discovered:
+                        files_found = True
+                        if not glob(new_home):
+                            os.mkdir(new_home)
+                    all_files.append(file)
+            for file in all_files:
+                os.startfile(file, 'print')
+            time.sleep(10)
+            for file in all_files:
+                shutil.move(file, new_home)
