d46c5dd099dbd09fa661a8d6364acca0f597954d
David J. Hammaker <davidjosh93@gmail.com>
2019-01-22 01:03:00 -0500

    Add conditional for timer
    10 second timer only operates if files are discovered (and thus need to 
    be printed).

--- pdf_printer.py
+++ pdf_printer.py
@@ -15,12 +15,13 @@ if __name__ == '__main__':
             for file in glob(f'{path}'[:-1] + '*'):
                 if file.endswith('.pdf'):
                     if not files_discovered:
-                        files_found = True
+                        files_discovered = True
                         if not glob(new_home):
                             os.mkdir(new_home)
                     all_files.append(file)
             for file in all_files:
                 os.startfile(file, 'print')
-            time.sleep(10)
+            if files_discovered:
+                time.sleep(10)
             for file in all_files:
                 shutil.move(file, new_home)
